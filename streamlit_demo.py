9442c97ef42a346fe066e810a4f0d73c5bf2e366
Orwell <karl_popper@139.com>
2022-10-26 18:16:56 +0800

    东京爱情故事

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
-
-st.title('小仙女，这后面网页我还没写，先放首歌听吧,待会换个音乐')
+st.title('小仙女，这后面网页我还没写，先放首歌听吧,待会换个音乐,')
+st.title("东京爱情故事")
 st.audio("460526429.mp3", format="audio/mp3", start_time=2)
