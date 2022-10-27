839b5aedd55734a15f53d4374c68acc6df1fb36d
Orwell <karl_popper@139.com>
2022-10-27 21:21:55 +0800

    版本1

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,40 +1,43 @@
 
 import streamlit as st
 import matplotlib.pyplot as plt
-import datetime as dt
 import pandas as pd
-import matplotlib.pyplot as plt
-import time
-from pytdx.exhq import TdxExHq_API, TDXParams
-api = TdxExHq_API(heartbeat=True)
-import math
 import numpy as np
+from pytdx.exhq import TdxExHq_API, TDXParams
 
-future_info=pd.read_csv("select_list.csv",index_col=0)
+
+
+
+
+api = TdxExHq_API(heartbeat=True)
+
+
+
+future_info = pd.read_csv("select_list.csv", index_col=0)
 
 def get_day_minute(name):
-    with api.connect("202.106.83.241",7721):
-        market_code,code=future_info[future_info["name"]==name][["market","code"]].values[0][:]
-        market_data=pd.DataFrame(api.get_minute_time_data(market_code,code))
+    with api.connect("202.106.83.241", 7721):
+        market_code, code = future_info[future_info["name"] == name][[
+            "market", "code"]].values[0][:]
+        market_data = pd.DataFrame(api.get_minute_time_data(market_code, code))
     return market_data
 
 
-option = st.selectbox(
-    '请选择品种',
-    set(future_info["name"].values) )
+st.title("欢迎进入神经元策略系统")
+st.write("算法结果仅供参考，依据算法结果进行决策风险自担")
+NQS_list=['神经元NQ_S1','神经元NQ_S2','神经元NQ_S3']
+option = st.sidebar.selectbox("查看算法详情",NQS_list)
 
-st.write(option)
-market_data=get_day_minute(option)
-
-
-if len (market_data)>0:
-    price_data=market_data["price"].values
+if option in NQS_list:
+    s_data=np.cumprod(  1+(  np.random.random(100)-0.3  )/100   )
+    fig = plt.figure(figsize=[10,5])          
+    ax = fig.add_subplot(1,1,1) 
     plt.style.use("dark_background")
-    fig, ax = plt.subplots()
-    ax.plot(price_data)
+    ax.plot(s_data)
     plt.grid()
     st.pyplot(fig)
-else:
-    st.write("暂无数据")
+    st.sidebar.write("策略净值",round(s_data[-1],2))
+    st.sidebar.write("仓位状况")
+
 st.write("听首歌放松下")
 st.audio("460526429.mp3",format='audio/mp3',start_time=0)
